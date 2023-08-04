c933d6ed222b8d8be9dd97752272ace638fb11b9
Joe <>
2023-08-04 11:14:26 +0800

    feat:添加文件

---
+++ src/views/app/sysConfig.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+
+<script setup name="sysConfig">
+import {getAuthRole, updateAuthRole} from "@/api/system/user";
+
+const route = useRoute();
+const {proxy} = getCurrentInstance();
+console.log(route,111)
+console.log(proxy,111)
+</script>
+
+<style scoped>
+
+</style>
